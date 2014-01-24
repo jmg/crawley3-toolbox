0d0e37177ff14b331206a234480cae37f05cc131
bossiernesto <bossi.ernestog@gmail.com>
2014-01-23 21:37:48 -0300

    added new persistance to python 3

---
+++ src/persistance/documents/__init__.py
@@ -0,0 +1,4 @@
+from .csv_doc import CSVDocument, session as csv_session
+from .json import JSONDocument,session as json_session
+from .xml import XMLDocument, session as xml_session
+from .json import session as json_session, JSONDocument
